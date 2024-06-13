bf13c18c9bbd3576aa9dabcce1192dfb20d5e8d7
TheGAzed <gameplay.nano@gmail.com>
2024-06-13 21:00:51 +0200

    rewind test

--- test/blackbox/suites/easy/easy_unset.c
+++ test/blackbox/suites/easy/easy_unset.c
@@ -15,7 +15,8 @@ TEST easy_unset_one(void) {
 
     FILE * fp = fopen(get_settings_singleton()->filepath, "rb");
     ASSERTm("COULDN'T OPEN FILE", fp);
-
+    rewind(fp);
+    
     Kakuro board = init_kakuro(fp);
     print_board(board);
     fclose(fp);
