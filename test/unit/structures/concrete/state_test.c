38072143356a060e9095b3890036443d7bd71a83
Matej Dedina <gameplay.nano@gmail.com>
2024-09-08 15:16:08 +0200

    fixed ASSERTm error

--- test/unit/structures/concrete/state_test.c
+++ test/unit/structures/concrete/state_test.c
@@ -5,8 +5,8 @@
 TEST is_one_value_all_valid_states(void) {
     for (size_t i = 0; i < MAX_BLOCK_VALUES; ++i) {
         ASSERTm(
-            is_one_value(1 << i),
-            "[ERROR] EXPECTED STATE TO BE A ONE VALUE"
+            "[ERROR] EXPECTED STATE TO BE A ONE VALUE",
+            is_one_value(1 << i)
         );
     }
     PASS();
@@ -15,8 +15,8 @@ TEST is_one_value_all_valid_states(void) {
 TEST is_one_value_invalid_state(void) {
     for (size_t i = 0; i < MAX_BLOCK_VALUES; ++i) {
         ASSERTm(
-            is_one_value(1 << i),
-            "[ERROR] EXPECTED STATE TO BE A ONE VALUE"
+            "[ERROR] EXPECTED STATE TO BE A ONE VALUE",
+            is_one_value(1 << i)
         );
     }
     PASS();
